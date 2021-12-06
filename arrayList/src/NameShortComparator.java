87072886a6ad5e254d2680e591b6fa12d8332b40
IvanOnukov <ivanonukov@ya.ru>
2021-12-06 21:47:07 +0300

    cleanup file

--- arrayList/src/NameShortComparator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class NameShortComparator implements Comparator<Student> {
-    public int compare(Student one, Student two){
-        return one.getName().compareTo(two.getName());
-    }
-}
